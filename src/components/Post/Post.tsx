3558df5e7f856ce4382fd0f54fc5ad3a7884e712
PranavJayachandran <pranavjay02@gmail.com>
2023-07-01 00:19:55 +0530

    Likes integrated

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -10,25 +10,106 @@ interface Props {
 const Post = ({ item }: Props) => {
   const [post, setPost] = useState<post>(item);
   const [liked_disliked_by_user, setliked_disliked_by_user] =
-    useState<number>(0);
+    useState<number>(-2);
+  let like_dislike_value = -2;
+
+  const postLikeDislike = async (prev: number) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      post_id: item.id,
+      value: like_dislike_value,
+      user_id: item.user_id,
+      prev: prev,
+    });
+
+    var requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/likesdislikes", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  const toggleLikeDislike = (prev: number) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      post_id: item.id,
+      value: like_dislike_value,
+      user_id: item.user_id,
+      prev: prev,
+    });
+
+    var requestOptions: RequestInit = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/likesdislikes", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
   const like = () => {
+    console.log("Called");
     if (liked_disliked_by_user == 1) {
       {
         setliked_disliked_by_user(liked_disliked_by_user - 1);
+        like_dislike_value = 0;
+        setPost((prevState) => ({ ...prevState, likes: post.likes - 1 }));
+        toggleLikeDislike(1);
       }
-    } else if (liked_disliked_by_user == 1) setliked_disliked_by_user(0);
-    else {
+    } else if (liked_disliked_by_user == -1) {
+      setliked_disliked_by_user(1);
+      like_dislike_value = 1;
+      setPost((prevState) => ({ ...prevState, dislikes: post.dislikes - 1 }));
       setPost((prevState) => ({ ...prevState, likes: post.likes + 1 }));
-      setliked_disliked_by_user(liked_disliked_by_user + 1);
+      toggleLikeDislike(-1);
+    } else if (liked_disliked_by_user == 0) {
+      like_dislike_value = 1;
+      setPost((prevState) => ({ ...prevState, likes: post.likes + 1 }));
+      setliked_disliked_by_user(1);
+      toggleLikeDislike(0);
+    } else {
+      like_dislike_value = 1;
+      setPost((prevState) => ({ ...prevState, likes: post.likes + 1 }));
+      setliked_disliked_by_user(1);
+      postLikeDislike(-2);
     }
   };
   const dislike = () => {
-    if (liked_disliked_by_user == -1) {
-      setliked_disliked_by_user(liked_disliked_by_user + 1);
-    } else if (liked_disliked_by_user == 1) setliked_disliked_by_user(0);
-    else {
+    if (liked_disliked_by_user == 1) {
+      setliked_disliked_by_user(-1);
+      like_dislike_value = -1;
+      setPost((prevState) => ({ ...prevState, likes: post.likes - 1 }));
       setPost((prevState) => ({ ...prevState, dislikes: post.dislikes + 1 }));
-      setliked_disliked_by_user(liked_disliked_by_user - 1);
+      toggleLikeDislike(1);
+    } else if (liked_disliked_by_user == -1) {
+      like_dislike_value = 0;
+      setliked_disliked_by_user(0);
+      setPost((prevState) => ({ ...prevState, dislikes: post.dislikes - 1 }));
+      toggleLikeDislike(-1);
+    } else if (liked_disliked_by_user == 0) {
+      like_dislike_value = -1;
+      setPost((prevState) => ({ ...prevState, dislikes: post.dislikes + 1 }));
+      setliked_disliked_by_user(-1);
+      toggleLikeDislike(0);
+    } else {
+      like_dislike_value = -1;
+      setPost((prevState) => ({ ...prevState, dislikes: post.dislikes + 1 }));
+      setliked_disliked_by_user(-1);
+      postLikeDislike(-2);
     }
   };
   useEffect(() => {
